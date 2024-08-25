9090b53481cb4aaeb73005d48eb0ceac20b125fd
MrSunfat <trannhatdanh15062001gmail.com>
2024-08-25 11:35:49 +0700

    feat: new client

---
+++ Auth.Client/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, CommonModule, MatButtonModule, NavbarComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+})
+export class AppComponent {
+    title = 'Auth.Client';
+}
